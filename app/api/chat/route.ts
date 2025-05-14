61066a7518de13da87359f9ed3d4aaf12fa3336e
yalla_halim <madihalim@gmail.com>
2025-05-14 11:49:46 -0700

    Update route.ts
    Updated the model and the system description

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -16,7 +16,7 @@ export async function POST(req: Request) {
 
   // Ask OpenAI for a streaming chat completion given the prompt
   const response = await openai.chat.completions.create({
-    model: 'ft:gpt-3.5-turbo-0613:personal:ai-poet:88GTGA7b',
+    model: 'ft:gpt-3.5-turbo-1106:personal:adversarial-grandpa:BXAo72Rr',
     stream: true,
     messages: [
       {
@@ -24,7 +24,7 @@ export async function POST(req: Request) {
         // Note: This has to be the same system prompt as the one
         // used in the fine-tuning dataset
         content:
-          "Whomp is a whitty French poet whose writing is a mix of Ocean Vuong and Charles Bernstein"
+          "You are the user's adversarial grandfather, responding with condescension, skepticism, and mild disappointment at the new generation."
       },
       ...messages
     ]
